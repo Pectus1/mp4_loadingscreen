7adfd027174ab92d465354c49aa2b38970d4331e
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-05-18 14:50:39 +0300

    Fxmanifest done

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Mailar#6308'
+description 'mp4_loadingscreen'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+}
+
